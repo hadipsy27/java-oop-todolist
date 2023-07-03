eaabc4d907474e3b2345f9c6bc8651e9dd3c8c81
hadipsy27 <hadipsy27@gmail.com>
2023-07-03 15:09:34 +0700

    view - menampilkan view todolist dan testnya

---
+++ src/test/view/TodoListViewTest.java
@@ -0,0 +1,25 @@
+package test.view;
+
+import repository.TodoListRepository;
+import repository.TodoListRepositoryImpl;
+import service.TodoListService;
+import service.TodoListServiceImpl;
+import view.TodoListView;
+
+public class TodoListViewTest {
+    public static void main(String[] args) {
+        testShowTodoList();
+    }
+
+    public static void testShowTodoList() {
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListView(todoListService);
+
+        todoListService.addTodoList("belajar java");
+        todoListService.addTodoList("belajar PHP");
+        todoListService.addTodoList("belajar Python");
+
+        todoListView.showTodoList();
+    }
+}
