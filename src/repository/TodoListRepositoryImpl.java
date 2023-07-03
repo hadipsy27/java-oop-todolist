e8cb0b108732ad5b65784663959a1467215e9fdc
hadipsy27 <hadipsy27@gmail.com>
2023-07-03 11:50:31 +0700

    Repository dan Service - menambahkan Todolist

--- src/repository/TodoListRepositoryImpl.java
+++ src/repository/TodoListRepositoryImpl.java
@@ -10,9 +10,43 @@ public class TodoListRepositoryImpl implements TodoListRepository{
         return data;
     }
 
+    public boolean isFull(){
+        // cek apakah model penuh?
+        var isFull = true;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == null){
+                isFull = false;
+                break;
+            }
+        }
+
+        return isFull;
+    }
+
+    public void resizeIsFull(){
+        // jika model penuh maka resize ke ukuran 2x lipat
+        if (isFull()){
+            var temp = data;
+            data = new Todolist[data.length * 2];
+
+            for (int i = 0; i < temp.length; i++) {
+                data[i] = temp[i];
+            }
+        }
+    }
+
     @Override
     public void add(Todolist todolist) {
 
+        resizeIsFull();
+
+        // tambahkan ke posisi yang array datanya NULL
+        for (int i = 0; i < data.length; i++) {
+            if(data[i] == null){
+                data[i] = todolist;
+                break;
+            }
+        }
     }
 
     @Override
